9c8d3b4daf63754327b24307eab16664a1d7eb37
prasad.s.deokar@gmail.com <prasad.s.deokar@gmail.com>
2022-11-16 11:24:12 +0530

    Adding springboot webservices project

---
+++ spring-boot-web-services/src/test/java/com/deepconcept/webservices/springbootwebservices/SpringBootWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepconcept.webservices.springbootwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
